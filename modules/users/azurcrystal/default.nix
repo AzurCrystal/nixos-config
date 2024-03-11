76ec8bab2b0d3b9ab7b3dae5f38e091ede84a113
AzurCrystal <azurcrystal@kiyohane.moe>
2024-03-11 09:45:57 +0800

    change(users): change user deploy key

--- modules/users/azurcrystal/default.nix
+++ modules/users/azurcrystal/default.nix
@@ -9,7 +9,7 @@
     home = "/home/azurcrystal";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7scDPGko5y8BMq+8qu/ezZmaocwFNDUB3NM/ygoHh4"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0Gna1mibX/uXKm7WBK5BrE5F8EF++lx86byIJQ7TTS"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKE7LQSv6+oqs2hcd4+t+1eYSe4HK5zufaOM7vxHtTmz"
     ];
     hashedPasswordFile = config.sops.secrets."users/azurcrystal/passwd".path;
     shell = pkgs.bash;
